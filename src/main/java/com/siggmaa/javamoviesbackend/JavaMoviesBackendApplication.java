4e0ef302417674b86f71544c67a300d9432639ed
Muhammad Sarim <Muhammadsarim203@gmail.com>
2023-06-11 00:18:03 +0500

    first commit

---
+++ src/main/java/com/siggmaa/javamoviesbackend/JavaMoviesBackendApplication.java
@@ -0,0 +1,22 @@
+package com.siggmaa.javamoviesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JavaMoviesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMoviesBackendApplication.class, args);
+
+	}
+
+	@GetMapping("/root")
+	public String apiRoot(){
+		return "Hellow sarim";
+	}
+
+}
