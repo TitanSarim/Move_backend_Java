43fde15abd1807f2d4d409b10cb10ded9f979b85
Muhammad Sarim <Muhammadsarim203@gmail.com>
2023-06-11 21:02:31 +0500

    variable setup

--- src/main/java/com/siggmaa/javamoviesbackend/JavaMoviesBackendApplication.java
+++ src/main/java/com/siggmaa/javamoviesbackend/JavaMoviesBackendApplication.java
@@ -1,5 +1,6 @@
 package com.siggmaa.javamoviesbackend;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class JavaMoviesBackendApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaMoviesBackendApplication.class, args);
