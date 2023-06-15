f0f9bda34250372d0c95c012616e6d907273788f
Muhammad Sarim <Muhammadsarim203@gmail.com>
2023-06-15 23:22:04 +0500

    first api

--- src/main/java/com/siggmaa/javamoviesbackend/JavaMoviesBackendApplication.java
+++ src/main/java/com/siggmaa/javamoviesbackend/JavaMoviesBackendApplication.java
@@ -3,6 +3,8 @@ package com.siggmaa.javamoviesbackend;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
